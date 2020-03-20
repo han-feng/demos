b359cd8f2482003fd8e7fda25b8bdb3aba906f97
han_feng <han_feng@foxmail.com>
2020-03-20 10:41:54 +0800

    完善Base64测试

--- demos-bc/src/test/java/demos/bc/BCTest.java
+++ demos-bc/src/test/java/demos/bc/BCTest.java
@@ -1,18 +1,29 @@
 package demos.bc;
 
-import java.io.UnsupportedEncodingException;
-
+import org.apache.commons.lang3.RandomStringUtils;
 import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.util.encoders.Hex;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BCTest {
 
 	@Test
-	public void testBinToString() throws UnsupportedEncodingException {
-		byte[] data = "Hello, Boun Cycastle!".getBytes("UTF-8");
+	public void testBinToString() {
+		testBase64("Hello, Boun Cycastle!");
+		for (int i = 0; i < 10; i++) {
+			testBase64(RandomStringUtils.randomAlphanumeric(10, 1000));
+		}
+	}
+
+	private void testBase64(String str) {
+		System.out.println("Test Base64: \"" + str + "\"");
+		byte[] data = str.getBytes();
+		String b64str = Base64.toBase64String(data);
 		System.out.println("[Hex] " + Hex.toHexString(data));
-		System.out.println("[B64] " + Base64.toBase64String(data));
+		System.out.println("[B64] " + b64str);
+		Assert.assertArrayEquals(data, Base64.decode(b64str));
+		System.out.println("OK!");
 	}
 
 }
